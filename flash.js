23f02f0723d7aa2790bf8c33c2a2415d84b004d3
hiroaki222 <hiroaki.takahara.222@gmail.com>
2024-04-08 01:07:44 +0900

    Separated the HTML and JS

--- flash.js
+++ flash.js
@@ -1,5 +1,19 @@
 document.addEventListener('DOMContentLoaded', function() {
   chrome.runtime.sendMessage({message: "FlashReader"}, function(response) {
-    document.getElementById('content').textContent = response.data;
+      document.getElementById('content').textContent = response.data;
   });
+
+  document.getElementById('skip-previous').addEventListener('click', skip_previous);
+  document.getElementById('play-circle').addEventListener('click', play_circle);
+  document.getElementById('skip-next').addEventListener('click', skip_next);
+
+  function skip_previous() {
+      console.log("skip_previous");
+  }
+  function play_circle() {
+      console.log("play_circle");
+  }
+  function skip_next() {
+      console.log("skip_next");
+  }
 });
